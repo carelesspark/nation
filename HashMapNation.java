37ed2a0a19c0ba7483fe139f70fcd86bf0075501
carelesspark <kiae1234@naver.com>
2023-09-12 19:21:13 +0900

    nation

---
+++ HashMapNation.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class HashMapNation {
+	public static void main(String[] args) {
+		HashMap<String, Integer> nations = new HashMap<String, Integer>();
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("나라 이름과 인구를 10개 입력하세요.(예: Korea 5000)");
+		while(true) { // "그만"이 입력될 때까지 반복
+			System.out.print("나라 이름, 인구 >> ");		
+			
+		}
+		
+		while(true) {
+			System.out.print("인구 검색 >> ");
+			
+		}
+		
+		scanner.close();
+	}
+
+}
